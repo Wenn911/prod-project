44a0e07ba993e533bbcdb379b94e94962993fee1
Wenn911 <Wenn123@yandex.ru>
2024-07-20 12:01:50 +0300

    change build plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -9,7 +9,7 @@ export function buildPlugins({
     paths,
     isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new webpack.ProgressPlugin(),
         new HtmlWebpackPlugin({
             template: paths.html,
@@ -21,9 +21,13 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new ReactRefreshWebpackPlugin(),
-        new BundleAnalyzerPlugin({
+    ]
+    if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin())
+        plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false,
-        })
-    ];
+        }))
+    }
+
+    return plugins;
 }
